8455eaead8f8967125f8f6201f01cf45e6a80684
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-08-17 14:41:03 +0530

    using bcrypt password encoder

--- src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,11 +25,17 @@ public class SecurityConfig {
         return http.build();
     }
 
-    //InMemoryUserDetails with no password encoder
+    //InMemoryUserDetails with bcrypt password encoder
+    //using a bcrypt generator generating the encoded password for 54321
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$DVI3hgLIhs8PgaXKsA3bw.6vnLoH4zascoCFoTNp0WS5TELhO/yC.").authorities("admin").build();
         return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
