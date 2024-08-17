ded67e1ceed0f258196ca58e02f12a329def164a
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-08-17 14:20:37 +0530

    in memory user details with no password encoder

--- src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.karanpaul.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,5 +21,13 @@ public class SecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    //InMemoryUserDetails with no password encoder
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
