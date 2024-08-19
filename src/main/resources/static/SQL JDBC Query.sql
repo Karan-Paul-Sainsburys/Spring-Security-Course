b9b62d831169a2d09c2700d6b563cfd87e026188
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-08-19 12:25:16 +0530

    setting up for customisation based authentication.

--- src/main/resources/static/SQL JDBC Query.sql
+++ src/main/resources/static/SQL JDBC Query.sql
@@ -2,3 +2,14 @@ create table users(username varchar(50) not null primary key,password varchar(50
 create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username,authority);
 
+CREATE TABLE customer (
+  id int GENERATED ALWAYS AS IDENTITY,
+  email varchar(45) NOT NULL,
+  pwd varchar(200) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+INSERT  INTO customer (email, pwd, role) VALUES ('happy@example.com', '{noop}EazyBytes@12345', 'read');
+INSERT  INTO customer (email, pwd, role) VALUES ('admin@example.com', '{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m', 'admin');
