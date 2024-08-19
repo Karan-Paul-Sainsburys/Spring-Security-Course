a26065d8e293d3c1550dad9d78c1836bdd82630c
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-08-19 12:11:28 +0530

    authentication setup using JDBCUserDetailsManager

--- src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,13 +27,10 @@ public class SecurityConfig {
         return http.build();
     }
 
-    //InMemoryUserDetails with bcrypt password encoder
-    //using a bcrypt generator generating the encoded password for 54321
+    //using the JDBCUserDetailsManager to authenticate the users from DB
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$DVI3hgLIhs8PgaXKsA3bw.6vnLoH4zascoCFoTNp0WS5TELhO/yC.").authorities("admin").build();
-        return new InMemoryUserDetailsManager(user,admin);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
