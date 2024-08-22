ac00bde89384e56fd16c864c27c8461b5d065899
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-08-22 09:12:43 +0100

    disabling csrf for post and put operations to be enabled

--- src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/karanpaul/springsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount","/myCard","/myBalance","/myLoans").authenticated()
                 .requestMatchers("/notices","/contacts","/welcome").permitAll());
         http.formLogin(withDefaults());
