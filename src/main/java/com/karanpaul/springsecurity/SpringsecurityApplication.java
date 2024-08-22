f8db12bd388ea95bcc5e64644f2d05bf0cfe9aeb
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-08-22 15:55:44 +0530

    to scan the repo and entity classes if written outside the main package

--- src/main/java/com/karanpaul/springsecurity/SpringsecurityApplication.java
+++ src/main/java/com/karanpaul/springsecurity/SpringsecurityApplication.java
@@ -2,7 +2,15 @@ package com.karanpaul.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/*
+* If the entity and the repository classes are written outside the main package.
+* then we can include the below annotations
+* */
+//@EnableJpaRepositories("com/lazy")
+//@EntityScan("com/entity")
 @SpringBootApplication
 public class SpringsecurityApplication {
 
