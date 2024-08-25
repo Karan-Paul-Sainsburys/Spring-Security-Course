5d2b712f6a3d477cdeedbd9bd912fd3824e53e16
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-08-25 08:28:37 +0100

    CustomAuthenticationEntryPoint body customisation

--- src/main/java/com/karanpaul/springsecurity/exceptionHandling/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/karanpaul/springsecurity/exceptionHandling/CustomAuthenticationEntryPoint.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     /**
@@ -23,7 +24,18 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     **/
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.setHeader("custom-spring-error", "custom error.Please check !");
-        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        LocalDateTime currentTimeStamp = LocalDateTime.now();
+        String message = (authException != null && authException.getMessage() != null) ? authException.getMessage()
+                : "Unauthorized";
+        String path = request.getRequestURI();
+        response.setHeader("eazybank-error-reason", "Authentication failed");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json;charset=UTF-8");
+        // Construct the JSON response
+        String jsonResponse =
+                String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}",
+                        currentTimeStamp, HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                        message, path);
+        response.getWriter().write(jsonResponse);
     }
 }
