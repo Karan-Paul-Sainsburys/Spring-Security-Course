889daf6f0b2e7c656600a504b05a3d848d3e7a4e
Karan.Paul <Karan.Paul@sainsburys.co.uk>
2024-10-02 12:40:02 +0100

    generating a JWT token

--- src/main/java/com/karanpaul/springsecurity/filter/JWTTokenGeneratorFilter.java
+++ src/main/java/com/karanpaul/springsecurity/filter/JWTTokenGeneratorFilter.java
@@ -1,12 +1,26 @@
 package com.karanpaul.springsecurity.filter;
 
+import com.karanpaul.springsecurity.constants.ApplicationsConstant;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
     /**
@@ -18,7 +32,26 @@ public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
      */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(null!=authentication){
 
+            //creating the secret key for signature
+            Environment environment = getEnvironment();
+            if(null!=environment){
+                String secret = environment.getProperty(ApplicationsConstant.JWT_SECRET_KEY, ApplicationsConstant.JWT_SECRET_DEFAULT_VALUE);
+                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                String jwtToken = Jwts.builder().issuer("Karan Paul")
+                        .subject("JWT Token")
+                        .claim("username",authentication.getName())
+                        .claim("authorities",authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
+                        .issuedAt(new Date())
+                        .expiration(new Date((new Date()).getTime()+300000000))
+                        .signWith(secretKey)
+                        .compact();
+                response.setHeader(ApplicationsConstant.JWT_HEADER,jwtToken);
+            }
+            filterChain.doFilter(request,response);
+        }
     }
 
     @Override
